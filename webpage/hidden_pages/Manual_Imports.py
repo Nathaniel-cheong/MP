26e4e1e2fbc36ef169fecc42aef48184056d6e05
Nathaniel-cheong <Nathanielcheong123@gmail.com>
2025-05-15 14:14:46 +0800

    Add files via upload

--- webpage/hidden_pages/Manual_Imports.py
+++ webpage/hidden_pages/Manual_Imports.py
@@ -1,1 +1,5 @@
+from imports import *
 
+def render():
+    st.title("Manuals Import")
+    st.write("This page is visible only after login.")
--- webpage/hidden_pages/RFQ_Dashboard.py
+++ webpage/hidden_pages/RFQ_Dashboard.py
@@ -1,1 +1,4 @@
-
+from imports import *
+def render():
+    st.title("RFQ")
+    st.write("This page is visible only after login.")
