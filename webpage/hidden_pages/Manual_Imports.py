a92b784d5464acab3076613fd01194c72a54a260
Nathaniel-cheong <Nathanielcheong123@gmail.com>
2025-05-20 11:16:20 +0800

    Delete webpage directory

--- webpage/hidden_pages/Manual_Imports.py
+++
@@ -1,5 +0,0 @@
-from imports import *
-
-def render():
-    st.title("Manuals Import")
-    st.write("This page is visible only after login.")
--- webpage/hidden_pages/RFQ_Dashboard.py
+++
@@ -1,4 +0,0 @@
-from imports import *
-def render():
-    st.title("RFQ")
-    st.write("This page is visible only after login.")
