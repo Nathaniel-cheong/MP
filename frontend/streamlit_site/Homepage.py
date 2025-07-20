c9690b45068be19f970aaf1eea6df48e15aecc17
User <dominokid472@gmail.com>
2025-07-20 19:59:26 +0800

    unable to hide icons

--- frontend/streamlit_site/Homepage.py
+++ frontend/streamlit_site/Homepage.py
@@ -31,13 +31,6 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-hide_github_icon = """
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-.styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; }
-footer { visibility: hidden; } header { visibility: hidden; }
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
 # ─── COOKIE SETUP ──────────────────────────────────────────────────────────
 cookies = EncryptedCookieManager(
     prefix="my_app/",
